a0fbea8f3b8efefe26288bd74750a7b717b225d9
mr-harshtyagi <writetoharshtyagi@gmail.com>
2022-05-01 11:10:09 +0530

    update

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -92,7 +92,7 @@ export default function Dish(props){
                     alt="dish-img"
                   ></img>
                 </Col>
-                <Col xs={5} lg={8} md={8}>
+                <Col xs={4} lg={8} md={8}>
                   <div style={{ marginTop: "9px" }}>
                     <h3 style={{ fontWeight: "600" }}>{props.name}</h3>
 
@@ -101,7 +101,7 @@ export default function Dish(props){
                     </h6>
                   </div>
                 </Col>
-                <Col xs={3} lg={2} md={2}>
+                <Col xs={4} lg={2} md={2}>
                   <div
                     className="form-check form-switch"
                     style={{
